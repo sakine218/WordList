85b41ae0c1e574edb3bd501e7ba899c1c1c9dab9
sakine218 <snishibayashi218@gmail.com>
2019-03-31 14:57:23 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Sakine Nishibayashi on 2019/03/31.
+//  Copyright © 2019 Sakine Nishibayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WordListTests/WordListTests.swift
@@ -0,0 +1,34 @@
+//
+//  WordListTests.swift
+//  WordListTests
+//
+//  Created by Sakine Nishibayashi on 2019/03/31.
+//  Copyright © 2019 Sakine Nishibayashi. All rights reserved.
+//
+
+import XCTest
+@testable import WordList
+
+class WordListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
